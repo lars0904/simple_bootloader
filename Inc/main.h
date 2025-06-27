f60047e0094908de6bd5c1e43194c53cc006f626
Lars <lars.wiggers@outlook.com>
2025-06-28 00:39:04 +0200

    moved CubeMX generated function definitions from main.c to main.h

--- Inc/main.h
+++ Inc/main.h
@@ -64,6 +64,9 @@ extern "C" {
 
 /* Exported functions prototypes ---------------------------------------------*/
 void Error_Handler(void);
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
 
 /* USER CODE BEGIN EFP */
 
